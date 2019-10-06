570e13b1af6e9381164c23ab22fd3035c7ddaa9e
Thomas-Home <tombye_07@hotmail.com>
2019-10-06 16:26:44 +1100

    Push 12

---
+++ Assets/Environments/Grass/MobileProWater/Water Sea/Scripts/Example/FPS.cs
@@ -0,0 +1,31 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+public class FPS : MonoBehaviour
+{
+	float deltaTime = 0.0f;
+	public int Fontsize = 1;
+	public Color TextColor = Color.white;
+
+	void Update ()
+	{
+		deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
+	}
+
+	void OnGUI ()
+	{
+		int w = Screen.width/2, h = Screen.height/2;
+
+		GUIStyle style = new GUIStyle ();
+
+		Rect rect = new Rect (40, 40, w, h * 2 / 30);
+		style.alignment = TextAnchor.UpperLeft;
+		style.fontSize = h * Fontsize / 30;
+		style.normal.textColor = TextColor;
+		float msec = deltaTime * 1000.0f;
+		float fps = 1.0f / deltaTime;
+		string text = string.Format ("{0:0.0} ms ({1:0.} fps)", msec, fps);
+		GUI.Label (rect, text, style);
+	}
+}
